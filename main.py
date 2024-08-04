923d37a333e038ea98d72b8fed43fa282eb0926a
Gijs Mos <gijs.mos@matanga.nl>
2024-08-04 15:58:19 +0200

    Set up a skeleton for the budget tracker.
    - First cut for the main function that will drive the tracker.
    - A greeting function to greet the user.  In this first version the user is hard coded to be Aaron.
    - Standard idiom for automatically starting the main function when executing as a script added.

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
+def greeting():
+	"""
+	Prints a greeting message to the user.
+	"""
+	print("Hello Aaron, this is your budget tracker.")
 
-print ("Aaron")
+def main():
+	"""
+	Greets the user and runs the budget tracker.
+	"""
+	greeting()
+	...
+
+if __name__ == "__main__":
+	main()
